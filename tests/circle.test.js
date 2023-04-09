c31bd211409a4538eee2ce0b9a6d01e9cdaee552
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-04-09 18:55:14 -0500

    revise square test, test pass

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -2,9 +2,9 @@ const Circle = require('../lib/Circle.js');
 
 describe('Circle', () => {
     it('should return logo with user choice of shape color, tex, and text color', () => {
-            const circle = new Circle('black','EKO','yellow');
-            expect((circle.shapeColor)).toEqual('black');
-            expect((circle.text)).toEqual('EKO');
-            expect((circle.textColor)).toEqual('yellow');
+        const circle = new Circle('black','EKO','yellow');
+        expect((circle.shapeColor)).toEqual('black');
+        expect((circle.text)).toEqual('EKO');
+        expect((circle.textColor)).toEqual('yellow');
     });
 });
