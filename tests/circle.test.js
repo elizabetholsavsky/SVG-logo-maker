b56febd4b64c6e576a4ed9a5f905a6a5fbc5e9c4
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-04-10 14:26:23 -0500

    update circle test to include render() method

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -2,9 +2,15 @@ const Circle = require('../lib/Circle.js');
 
 describe('Circle', () => {
     it('should return logo with user choice of shape color, tex, and text color', () => {
-        const circle = new Circle('black','EKO','yellow');
-        expect((circle.shapeColor)).toEqual('black');
-        expect((circle.text)).toEqual('EKO');
-        expect((circle.textColor)).toEqual('yellow');
+        const circle = new Circle('blue','EKO','white');
+        expect(circle.render()).toEqual(`
+        <svg version="1.1"
+            width="300" height="200"
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="150" cy="100" r="100" fill="blue" />
+            <text x="150" y="125" font-size="70" text-anchor="middle" fill="white">EKO</text>
+        </svg>
+        `
+        );
     });
 });
