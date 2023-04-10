2e9f03f1dbbf770439cfee78282f33d372633b1e
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-04-10 14:29:52 -0500

    update square test to include render() method

--- tests/square.test.js
+++ tests/square.test.js
@@ -2,9 +2,15 @@ const Square = require('../lib/Square.js');
 
 describe('Square', () => {
     it('should return logo with user choice of shape color, tex, and text color', () => {
-        const square = new Square('red','EKO','white');
-        expect((square.shapeColor)).toEqual('red');
-        expect((square.text)).toEqual('EKO');
-        expect((square.textColor)).toEqual('white');
+        const square = new Square('red','EKO','yellow');
+        expect(square.render()).toEqual(`
+        <svg version="1.1"
+            width="300" height="200"
+            xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="200" fill="red"/>
+            <text x="100" y="125" font-size="70" text-anchor="middle" fill="yellow">EKO</text>
+        </svg>
+        `
+        );
     });
 });
