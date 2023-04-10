96fdd1d413fc4d631f151122119d5e62c8f7d674
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-04-10 14:34:34 -0500

    update triangle test to include render() method

--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -2,9 +2,15 @@ const Triangle = require('../lib/Triangle.js');
 
 describe('Triangle', () => {
     it('should return logo with user choice of shape color, tex, and text color', () => {
-        const triangle = new Triangle('blue','EKO','orange');
-        expect((triangle.shapeColor)).toEqual('blue');
-        expect((triangle.text)).toEqual('EKO');
-        expect((triangle.textColor)).toEqual('orange');
+        const triangle = new Triangle('lime','EKO','aliceblue');
+        expect(triangle.render()).toEqual(`
+        <svg version="1.1" 
+            width="300" height="200"
+            xmlns="http://www.w3.org/2000/svg">
+            <polygon points="100, 15 200, 200 0, 200" fill="lime"/>
+            <text x="100" y="185" font-size="70" text-anchor="middle" fill="aliceblue">EKO</text>
+        </svg>
+        `
+        );
     });
 });
