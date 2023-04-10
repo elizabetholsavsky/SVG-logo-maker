edf863394c3a33669a3b682f540f2f569e382c1b
Elizabeth Olsavsky <elizabetholsavsky@gmail.com>
2023-04-10 14:15:03 -0500

    add comments to all files

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const questions = require('./lib/questions.js');
 const fileName = "./examples/logo.svg";
 const setShape = require('./lib/setShape.js')
 
+// function to create new svg file using inquirer response and file system
 function createLogo(response) {
     const svg = setShape(response);
     fs.writeFile(fileName, svg, ()=> console.log('Generated logo.svg'));
 }
 
+// initialize, ask questions then createLogo using responses, catch any errors
 function init() {
     inquirer 
     .prompt(questions)
